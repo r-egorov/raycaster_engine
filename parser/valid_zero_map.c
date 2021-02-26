90ce31d20a9234297795a20ca786bbf4bc446ab1
Chansey Isis <cisis@am-b4.msk.21-school.ru>
2021-02-26 11:20:23 +0300

    Get rid of SEGFAULT when zero in corners of the map

--- parser/valid_zero_map.c
+++ parser/valid_zero_map.c
@@ -6,7 +6,7 @@
 /*   By: cisis <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 17:59:52 by cisis             #+#    #+#             */
-/*   Updated: 2021/02/25 19:11:28 by cisis            ###   ########.fr       */
+/*   Updated: 2021/02/26 11:18:08 by cisis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,22 @@ int			valid_zero_map(t_parsed *parsed, int x, int y)
 
 	line_len = ft_strlen((parsed->map)[y]);
 	if ((y == 0) || (y == (int)parsed->map_height - 1) ||
-		(x == 0) || (x == line_len))
+		(x == 0) || (x == line_len - 1))
 		g_errno = 6;
 	i = -1;
-	while (i < 2)
+	while ((i < 2) && !(g_errno))
 	{
 		j = -1;
-		while (j < 2)
+		while ((j < 2) && !(g_errno))
 		{
 			if ((!((i == 0) && (j == 0))) &&
 				(!(valid_surroudings((parsed->map)[y + i][x + j]))))
 				g_errno = 6;
-			if (g_errno)
-				return (0);
 			j++;
 		}
 		i++;
 	}
+	if (g_errno)
+		return (0);
 	return (1);
 }
